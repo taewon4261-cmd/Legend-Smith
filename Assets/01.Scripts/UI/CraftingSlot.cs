30894add72e443a0dbd8ddd19dcb8cb26e3f779d
Teca <taewon4261@gmail.com>
2026-01-30 17:25:14 +0900

    Feat 아이템 잠금 기능 구현 및 무기 스프라이트 추가
    아이템 잠금/해제 상태 로직 추가
    (잠금 상태 시 UI 반투명 처리 시각 효과 적용)
    
    신규 무기 이미지 스프라이트 리소스 추가

--- Assets/01.Scripts/UI/CraftingSlot.cs
+++ Assets/01.Scripts/UI/CraftingSlot.cs
@@ -1,12 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CraftingSlot : MonoBehaviour
 {
-    [Header("데이터 연결")]
+    [Header("데이터")]
     public ItemDataSO itemData;
     public SmithingUI smithingUI;
+
+    [Header("UI")]
+    public Image Icon;
+    public Button button;
+
+    private readonly Color lockedColor = new Color(0, 0, 0, 0.5f); // Color은 구조체라 GC 관여없음
+    private readonly Color unlockedColor = Color.white;
+
+    [Header("잠금 설정")]
+    public bool isUnlocked;
+
+    private void Start()
+    {
+        if(Icon == null) Icon = GetComponent<Image>();
+        if(button == null) button = GetComponent<Button>();
+      
+
+        if (isUnlocked)
+        {
+            UnlockSlot(); // 체크되어 있으면 해금 상태로 시작
+        }
+        else
+        {
+            LockSlot();   // 아니면 잠금
+        }
+    }
+
+    public void LockSlot()
+    {
+        Icon.color = lockedColor;
+        button.interactable = false; // 클릭 불가
+        isUnlocked = false;
+    }
+
+    public void UnlockSlot()
+    {
+        Icon.color = unlockedColor;
+        button.interactable = true;
+        isUnlocked = true;
+    }
 
     public void OnClickSelectThis()
     {
